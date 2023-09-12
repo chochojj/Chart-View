7ba2edd743233dd561decb9643cdb822d0afa917
chochojj <csgy114@gmail.com>
2023-09-12 13:44:09 +0900

    chore: 차트 라이브러리 설치

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+import { reset } from 'styled-reset'
+
+const GlobalStyles = createGlobalStyle`
+${reset},
+    *,
+    *::before,
+    *::after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html, body {
+        height: 100%;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+        
+    }
+    body {
+        
+    }
+    a {
+        color: inherit;
+        text-decoration: none;
+        font-size: inherit;
+    }
+    button, 
+    input, 
+    textarea, 
+    select, 
+    option {
+        font-family: inherit;
+        color: inherit;
+        font-size: inherit;
+        border: 0;
+        outline: 0;
+        background-color: transparent;
+    }
+    button:focus, 
+    input:focus, 
+    textarea:focus, 
+    select:focus{
+        outline: none;
+    }
+`
+
+export default GlobalStyles
